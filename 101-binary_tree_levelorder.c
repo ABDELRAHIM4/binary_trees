7e351e3b3219d663cc148560bfe075a1c8939b5b
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-03-27 19:26:52 -0700

    binary_tree

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	while (tree->parent != NULL)
+	{
+		tree = tree->parent;
+	}
+	while (tree->left != NULL)
+	{
+		if (tree->right == NULL)
+			return 
+		return (tree);
+		tree->
+	}
+
