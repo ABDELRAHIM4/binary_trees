c1c110d3b136bf96ca167b055d86689cce526f48
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-03 20:33:26 -0700

    binary_tree

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,19 +1,32 @@
 #include "binary_trees.h"
+/**
+ * depth_t - Return the depth
+ *@node : the node
+ *Return: the depth
+ */
 size_t depth_t(const binary_tree_t *node)
 {
 	size_t dep = 0;
+
 	if (node == NULL)
 		return (dep);
-	while(node->parent)
+	while (node->parent)
 	{
 		node = node->parent;
-		dep ++;
+		dep++;
 	}
 	return (dep);
 }
-int binary_t(const binary_tree_t * ans, const binary_tree_t *node)
+/**
+ *  binary_t - if the current equal to node
+ * @ans: the current
+ *@node: the node
+ *Return: the result of comparing
+*/
+int binary_t(const binary_tree_t *ans, const binary_tree_t *node)
 {
 	const binary_tree_t *current = node;
+	
 	while (current)
 	{
 		if (ans == current)
@@ -24,6 +37,12 @@ int binary_t(const binary_tree_t * ans, const binary_tree_t *node)
 	}
 	return (0);
 }
+/**
+ * binary_trees_ancestor - Finds the lowest ancestor
+ *@first: pointer to the first node
+ *@second: pointer to the second node
+ *Return: the parent of the nodes
+*/
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
 	size_t depth_f = 0, depth_s = 0;
@@ -41,7 +60,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 		if (depth_f > depth_s)
 		{
 			current = current->parent;
-			depth_f --;
+			depth_f--;
 		}
 		else
 		{
@@ -49,7 +68,8 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 			depth_s--;
 		}
 	}
-	while(current && !(binary_t(current, first)) && !(binary_t(current, second)))
+	while (current && !(binary_t(current, first)) && 
+			!(binary_t(current, second)))
 			current = current->parent;
 	return (current);
 }
