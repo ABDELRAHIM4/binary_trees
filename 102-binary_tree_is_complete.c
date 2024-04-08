bae1ce3cc6f0fb20b421f6515f1c8aa3b10f6c58
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-07 20:59:43 -0700

    binary_tree

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,24 +1,21 @@
 #include "binary_trees.h"
-/*
- * check - check the node
+/**
+ * check - To make sure if  the tree is complete
  *@current: pointer to node
- *Return: 1 if complete
+ *Return: return 1 if complete 0 otherwise
  */
 int check(const binary_tree_t *current)
 {
 	if (current == NULL)
-                return (0);
-       	if (current->left == NULL && current->right != NULL)
-        	return (0);
-        if (current->left != NULL && current->right == NULL)
-                return(0);
-	if (current->left != NULL && current->right->left!= NULL)
-                {
-                        current = current->right;
-                }
-        else
-        	current = NULL;
-
+		return (0);
+	if (current->left == NULL && current->right != NULL)
+		return (0);
+	else if (current->left != NULL && current->right == NULL)
+		return (0);
+	else if (current->left != NULL && current->right->left != NULL)
+		current = current->right;
+	else
+		current = NULL;
 	return (1);
 }
 /**
@@ -30,33 +27,33 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	const binary_tree_t *second = tree;
 	const binary_tree_t *current = tree;
-        int com = 1;
-	
+	int com = 1;
+
 	if (tree == NULL)
 		return (0);
-	while (current)
+	while (current != NULL)
 	{
-			if (check(current->left) == 0 || check(current->right) == 0)
-				com = 0;
-			if (current->left != NULL && current->right != NULL)
-                                com = 1;
-        		if (current->left == NULL && current->right == NULL)
-                		com = 1;
-        		else
-                		com = 0;
-			current = current->left;
+		if (check(current->left) == 0 || check(current->right) == 0)
+			com = 0;
+		else if (current->left != NULL && current->right != NULL)
+			com = 1;
+		else if (current->left == NULL && current->right == NULL)
+			com = 1;
+		else
+			com = 0;
+		current = current->left;
 	}
-	while (second)
+	while (second != NULL)
 	{
-			if (check(second->left) == 0 || check(second->right) == 0)
-                                com = 0;
-                        if (second->left != NULL && second->right != NULL)
-                                com = 1;
-                        if (second->left == NULL && second->right == NULL)
-                                com = 1;
-                        else
-                                com = 0;
-        	second = second->left;
+		if (check(second->left) == 0 || check(second->right) == 0)
+			com = 0;
+		if (second->left != NULL && second->right != NULL)
+			com = 1;
+		if (second->left == NULL && second->right == NULL)
+			com = 1;
+		else
+			com = 0;
+		second = second->left;
 	}
 	return (com);
 }
